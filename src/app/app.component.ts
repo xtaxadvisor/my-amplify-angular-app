fb61c21478bf8bd353d963fee17f9354f7da88bd
xtaxadvisor <gdoffice@mail.com>
2024-11-22 15:19:35 +0000

    changes 1121

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,14 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TodosComponent } from './todos/todos.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, TodosComponent],
-  get   () {
-    return this.imports;
-  },
-  set imports(value) {
-    this._imports = value;
-  },
-  schemas: [CUSTOM_ELEMENTS_SCHEMA] // Add this line to allow custom elements
+  imports: [TodosComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // Allows usage of custom elements in the template
 })
 export class AppComponent {
-  title = 'amplify-angular-template';
+  title = 'amplify-angular-template'; // Title of the application
 }
